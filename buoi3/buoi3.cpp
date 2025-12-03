3a9ed767ee99497591785ee5a469ba8722e8bafe
buoi2 <Administrator@PMT2045>
2025-12-03 15:19:23 +0700

    BT 3

--- buoi3/buoi3.cpp
+++ buoi3/buoi3.cpp
@@ -19,7 +19,16 @@ void cau2() {
 	cout << "Thuc lanh: " << (luong + phucap) * 0.9 << endl;
 
 }
+void cau3() {
+	float cannang, chieucao;
+	cout << "Nhap can nang (kg): ";
+	cin >> cannang;
+	cout << "Nhap chieu cao (m): ";
+	cin >> chieucao;
+	cout << "BMI: " << cannang / (chieucao * chieucao) << endl;
+}
 
-int main() {
-	cau2();
+int main() 
+{
+	cau3();
 }
