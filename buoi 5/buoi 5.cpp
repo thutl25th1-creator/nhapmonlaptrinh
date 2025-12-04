c2511901c5e57b24b2dec6747b92cc64d97bb416
buoi5 <Administrator@PMT2045>
2025-12-04 12:56:26 +0700

    bai tap 3

--- buoi 5/buoi 5.cpp
+++ buoi 5/buoi 5.cpp
@@ -25,9 +25,23 @@ void cau2(){
     else
         cout << "khac dau";
     }
+void cau3() {
+    int a, b;
+    cout << "Nhap a, b: ";
+    cin >> a >> b;
+    if (a == 0) {
+        if (b == 0)
+            cout << "Phuong trinh vo so nghiem" << endl;
+        else
+            cout << " Phuong trinh vo nghiem" << endl;
+    }
+    else {
+        cout << "nghiem la: " << (-b * 1.0 / a) << endl;
+    }
+}
 int main()
 {
-    cau2();
+    cau3();
 
 }
 
