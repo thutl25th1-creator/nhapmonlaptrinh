f696662fa8a6feab0c37ac4dd4cf081955d16f3a
bai tap <Administrator@PMT2045>
2025-12-17 13:58:07 +0700

    bai tap 2

--- buoi 7/buoi 7.cpp
+++ buoi 7/buoi 7.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "SoNguyen.h"
+using namespace std;
+
 int main()
 {
-	int n = 5;
-	bool kq = NguyenTo(n);
+	int n = 1026;
+	int kq = DemChuSo(n);
+	cout << "So chu so: " << kq << endl;
 }
 
