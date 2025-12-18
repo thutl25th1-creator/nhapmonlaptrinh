fe14929f263ee660817415958a14ea3700253fda
baitap <Administrator@PMT2045>
2025-12-18 14:06:38 +0700

    bai tap1

--- buoi8/Array.cpp
+++ buoi8/Array.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void input(int b[], int& n) {
+void input(int b[], int & n) {
 	cout << "Nhap so luong phan tu: ";
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cout << "\t+ Phan tu " << (i + 1) << ": ";
 		cin >> b[i];
 	}
+}
+void output(int b[], int n) {
+	cout << "Danh sach phan tu mang:" << endl;
+	int i = 0;
+	while (i < n) {
+		cout << b[i] << "   ";
+		i++;
+	}
+	cout << endl;
 }
--- buoi8/buoi8.cpp
+++ buoi8/buoi8.cpp
@@ -5,5 +5,6 @@ int main()
 {
 	int a[MAX], n = 0;
 	input(a, n);
+	output(a, n);
 }
 
